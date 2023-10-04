bcce747e7401654fd9d58d341b62f1cd6838f83e
jacobfreedman <jacob.freedman1@gmail.com>
2023-10-04 12:19:23 -0400

    Filtered tracker location

--- ArgosTrackingTool.py
+++ ArgosTrackingTool.py
@@ -41,10 +41,13 @@ for lineString in line_list:
     obs_lat = lineData[6]
     obs_lon = lineData[7]
 
-    #Add items to dictionaries
-    date_dict[record_id] = obs_date
-    location_dict[record_id] = (obs_lat, obs_lon)
+    #Determine if location class criteria are met
+    if obs_lc in ("1", "2", "3"):
 
-    #Print the location of sara
-    #print(f"Record {record_id} indicates Sara was seen at lat:{obs_lat},lon:{obs_lon} on {obs_date}.")
+        #Add items to dictionaries
+        date_dict[record_id] = obs_date
+        location_dict[record_id] = (obs_lat, obs_lon)
+
+        #Print the location of sara
+        #print(f"Record {record_id} indicates Sara was seen at lat:{obs_lat},lon:{obs_lon} on {obs_date}.")
 
